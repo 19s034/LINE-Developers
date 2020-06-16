b3866eac6e6b64519d619305c50ae48f9d64efaf
DSP6101 <19s034hayate@gmail.com>
2020-06-16 11:17:33 +0900

    チーム萩原

--- main.py
+++ main.py
@@ -1,70 +1,65 @@
-from flask import Flask, request, abort
-
-from linebot import (
-   LineBotApi, WebhookHandler
-)
-from linebot.exceptions import (
-   InvalidSignatureError
-)
-from linebot.models import (
-   MessageEvent, TextMessage, TextSendMessage, ImageMessage, ImageSendMessage
-)
-
 import os
+from pathlib import Path
+
+from flask import Flask, abort, request
+
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import (ImageMessage, ImageSendMessage, MessageEvent,
+                            TextMessage, TextSendMessage)
 
 app = Flask(__name__)
 
 YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
 YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
 
-line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
-handler = WebhookHandler(YOUR_CHANNEL_SECRET)
+SRC_IMAGE_PATH = "static/images/{}.jpg"
+MAIN_IMAGE_PATH = "static/images/{}_main.jpg"
+PREVIEW_IMAGE_PATH = "static/images/{}_preview.jpg"
+
 
 @app.route("/")
 def hello_world():
-   return "hello world!"
+    return "hello world!"
 
-@app.route("/callback", methods=['POST'])
+
+@app.route("/callback", methods=["POST"])
 def callback():
-   # get X-Line-Signature header value
-   signature = request.headers['X-Line-Signature']
+    # get X-Line-Signature header value
+    signature = request.headers["X-Line-Signature"]
 
-   # get request body as text
-   body = request.get_data(as_text=True)
-   app.logger.info("Request body: " + body)
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
 
-   # handle webhook body
-   try:
-       handler.handle(body, signature)
-   except InvalidSignatureError:
-       print("Invalid signature. Please check your channel access token/channel secret.")
-       abort(400)
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return "OK"
 
-   return 'OK'
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-   line_bot_api.reply_message(
-       event.reply_token,
-       TextSendMessage(text=event.message.text))
+    line_bot_api.reply_message(
+        event.reply_token, TextSendMessage(text=event.message.text)
+    )
+
 
 @handler.add(MessageEvent, message=ImageMessage)
 def handle_image(event):
     message_id = event.message.id
 
-    # message_idから画像のバイナリデータを取得
-    message_content = line_bot_api.get_message_content(message_id)
+    src_image_path = Path(SRC_IMAGE_PATH.format(message_id)).absolute()
+    main_image_path = MAIN_IMAGE_PATH.format(message_id)
+    preview_image_path = PREVIEW_IMAGE_PATH.format(message_id)
 
-    with open(Path(f"static/images/{message_id}.jpg").absolute(), "wb") as f:
-        # バイナリを1024バイトずつ書き込む
-        for chunk in message_content.iter_content():
-            f.write(chunk)
+    # 画像を保存
+    save_image(message_id, src_image_path)
 
-@handler.add(MessageEvent, message=ImageMessage)
-def handle_image(event):
-
-    main_image_path = f"static/images/{event.message.id}_main.jpg"
-    preview_image_path = f"static/images/{event.message.id}_preview.jpg"
+    # 画像の加工、保存
+    date_the_image(src=src_image_path, desc=Path(main_image_path).absolute())
+    date_the_image(src=src_image_path, desc=Path(preview_image_path).absolute())
 
     # 画像の送信
     image_message = ImageSendMessage(
@@ -72,8 +67,21 @@ def handle_image(event):
         preview_image_url=f"https://date-the-image.herokuapp.com/{preview_image_path}",
     )
 
+    app.logger.info(f"https://date-the-image.herokuapp.com/{main_image_path}")
     line_bot_api.reply_message(event.reply_token, image_message)
 
+    # 画像を削除する
+    src_image_path.unlink()
+
+
+def save_image(message_id: str, save_path: str) -> None:
+    """保存"""
+    message_content = line_bot_api.get_message_content(message_id)
+    with open(save_path, "wb") as f:
+        for chunk in message_content.iter_content():
+            f.write(chunk)
+
+
 if __name__ == "__main__":
-   port = int(os.getenv("PORT"))
-   app.run(host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
