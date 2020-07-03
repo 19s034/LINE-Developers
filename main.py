c11e6ad596e215c0385514b00a71ac39545088e8
hayate <19s034hayate@gmail.com>
2020-07-03 10:31:17 +0900

    make it better

--- main.py
+++ main.py
@@ -101,12 +101,12 @@ def handle_image(event):
     # 画像比較用にfilter_sizeをファイル名に入れておく
     out.save('out/bokashi_{0}.jpg'.format(filter_size))
 
-    "static/" + message_id + ".jpg" = "out/bokashi_{0}.jpg"
-    
+    #"static/" + message_id + ".jpg" = "out/bokashi_{0}.jpg"
+
     # 画像の送信
     image_message = ImageSendMessage(
-        original_content_url="https://secret-lake-56663.herokuapp.com/sttic/" + message_id + ".jpg",
-        preview_image_url="https://secret-lake-56663.herokuapp.com/static/" + message_id + ".jpg",
+        original_content_url="https://secret-lake-56663.herokuapp.com/sttic/out/bokashi_{0}.jpg",
+        preview_image_url="https://secret-lake-56663.herokuapp.com/static/out/bokashi_{0}.jpg",
     )
 
     #app.logger.info("https://secret-lake-56663.herokuapp.com/static/{main_image_path}")
