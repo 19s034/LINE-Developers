292e77061f9e9941c6f0747e027e207fe1b96056
DSP6101 <19s034hayate@gmail.com>
2020-08-04 14:02:37 +0900

    hagi

--- main.py
+++ main.py
@@ -118,23 +118,23 @@ def handle_image(event):
     src = cv2.imread(fname)
     src_gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
 
-    faces = face_cascade.detectMultiScale(src_gray)
-
-    for x, y, w, h in faces:
-        #cv2.rectangle(src, (x, y), (x + w, y + h), (255, 0, 0), 2)
-        face = src[y: y + h, x: x + w]
-        face_gray = src_gray[y: y + h, x: x + w]
-        eyes = eye_cascade.detectMultiScale(face_gray)
-        for (ex, ey, ew, eh) in eyes:
-            cv2.rectangle(face, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
-
-    cv2.imwrite("static/gray.jpg", src)
+    #faces = face_cascade.detectMultiScale(src_gray)
+    eyes = eye_cascade.detectMultiScale(src_gray)
+    #for x, y, w, h in faces:
+    #    #cv2.rectangle(src, (x, y), (x + w, y + h), (255, 0, 0), 2)
+    #    face = src[y: y + h, x: x + w]
+    #    face_gray = src_gray[y: y + h, x: x + w]
+    #    eyes = eye_cascade.detectMultiScale(face_gray)
+    #    for (ex, ey, ew, eh) in eyes:
+    #        cv2.rectangle(face, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
+#
+    #cv2.imwrite("static/gray.jpg", src)
 
     #faces = face_cascade.detectMultiScale(src_gray)
 
     ratio = 0.05
 
-    for x, y, w, h in faces:
+    for x, y, w, h in eyes:
         small = cv2.resize(src[y: y + h, x: x + w], None, fx=ratio, fy=ratio, interpolation=cv2.INTER_NEAREST)
         src[y: y + h, x: x + w] = cv2.resize(small, (w, h), interpolation=cv2.INTER_NEAREST)
 
