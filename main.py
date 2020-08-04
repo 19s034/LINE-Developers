8f4e4702d42a05a845ac9b1abbed9d5ba040b531
DSP6101 <19s034hayate@gmail.com>
2020-08-04 13:45:20 +0900

    hagi

--- main.py
+++ main.py
@@ -16,6 +16,8 @@ app = Flask(__name__)
 
 
 face_cascade_path = "haarcascade_frontalface_default.xml"
+eye_cascade_path = "haarcascade_eye.xml"
+
 YOUR_CHANNEL_ACCESS_TOKEN = "d9TJaXv1KYVppBbVUrSZxF+XGe4N+IL9JtUpAA8VgJXk4OVmiYhiEgUMaDLq8Ic2idBZbBYcdwksMd3THXzSf5CHrLdtXi2i0Irq2iVaKw72MVNrKShwNr2JMQJ/yi0+XLA6hkEaE+0JqoXWzbDtTAdB04t89/1O/w1cDnyilFU="
 YOUR_CHANNEL_SECRET = "e0b5ca81f71074e9639844976085b288"
 
@@ -46,7 +48,6 @@ def callback():
     except InvalidSignatureError:
         abort(400)
     return "OK"
-
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
@@ -96,19 +97,48 @@ def handle_image(event):
    #cv2.imwrite("static/gray.jpg", img)
 #-------------------------------------------------------------------------
 
+
     
-    img = cv2.imread(fname) #画像を読み出しオブジェクトimgに代入
+    #img = cv2.imread(fname) #画像を読み出しオブジェクトimgに代入
+#
+    ##オブジェクトimgのshapeメソッドの1つ目の戻り値(画像の高さ)をimg_heightに、2つ目の戻り値(画像の幅)をimg_widthに代入
+    #img_height,img_width=img.shape[:2]
+#
+    #scale_factor=0.05 #縮小処理時の縮小率(小さいほどモザイクが大きくなる)
+    #img = cv2.resize(img,None,fx=scale_factor,fy=scale_factor) #縮小率の倍率で画像を縮小
+    ##画像を元の画像サイズに拡大。ここで補完方法に'cv2.INTER_NEAREST'を指定することでモザイク状になる
+    #img = cv2.resize(img, (img_width, img_height),interpolation=cv2.INTER_NEAREST)
+#
+    #cv2.imwrite("static/gray.jpg", img) #ファイル名'mosaic.png'でimgを保存
+#
 
-    #オブジェクトimgのshapeメソッドの1つ目の戻り値(画像の高さ)をimg_heightに、2つ目の戻り値(画像の幅)をimg_widthに代入
-    img_height,img_width=img.shape[:2]  
+    face_cascade = cv2.CascadeClassifier(face_cascade_path)
+    eye_cascade = cv2.CascadeClassifier(eye_cascade_path)
 
-    scale_factor=0.05 #縮小処理時の縮小率(小さいほどモザイクが大きくなる)
-    img = cv2.resize(img,None,fx=scale_factor,fy=scale_factor) #縮小率の倍率で画像を縮小
-    #画像を元の画像サイズに拡大。ここで補完方法に'cv2.INTER_NEAREST'を指定することでモザイク状になる
-    img = cv2.resize(img, (img_width, img_height),interpolation=cv2.INTER_NEAREST)
+    src = cv2.imread(fname)
+    src_gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
 
-    cv2.imwrite("static/gray.jpg", img) #ファイル名'mosaic.png'でimgを保存
+    faces = face_cascade.detectMultiScale(src_gray)
 
+    for x, y, w, h in faces:
+        cv2.rectangle(src, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        face = src[y: y + h, x: x + w]
+        face_gray = src_gray[y: y + h, x: x + w]
+        eyes = eye_cascade.detectMultiScale(face_gray)
+        for (ex, ey, ew, eh) in eyes:
+            cv2.rectangle(face, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
+
+    cv2.imwrite("static/gray.jpg", src)
+
+    faces = face_cascade.detectMultiScale(src_gray)
+
+    ratio = 0.05
+
+    for x, y, w, h in faces:
+        small = cv2.resize(src[y: y + h, x: x + w], None, fx=ratio, fy=ratio, interpolation=cv2.INTER_NEAREST)
+        src[y: y + h, x: x + w] = cv2.resize(small, (w, h), interpolation=cv2.INTER_NEAREST)
+
+    cv2.imwrite("static/gray.jpg", src)
 
     # 画像の送信
     image_message = ImageSendMessage(
