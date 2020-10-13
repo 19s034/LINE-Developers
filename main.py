b7cd64a9b7188f473148fdc7367f459c75349d66
19s034 <19s034hayate@gmail.com>
2020-10-13 00:38:44 +0000

    main

--- main.py
+++ main.py
@@ -138,21 +138,21 @@ def handle_image_message(event):
     
 
     
-#画像送信処理
-def handle_send_message(event):
-    #mozaiku(event)
-    result = change_image(event)
+# #画像送信処理
+# def handle_send_message(event):
+#     #mozaiku(event)
+#     result = change_image(event)
 
-    if result:
-        line_bot_api.reply_message(
-            event.reply_token, ImageSendMessage(
-                original_content_url=FQDN + "/static/" + event.message.id + "_face.jpg",
-                preview_image_url=FQDN + "/static/" + event.message.id + "_face.jpg",
-            )
-            )
+#     if result:
+#         line_bot_api.reply_message(
+#             event.reply_token, ImageSendMessage(
+#                 original_content_url=FQDN + "/static/" + event.message.id + "_face.jpg",
+#                 preview_image_url=FQDN + "/static/" + event.message.id + "_face.jpg",
+#             )
+#             )
 
-    else:
-        handle_textmessage(event)
+#     else:
+#         handle_textmessage(event)
 
 
 #画像送信処理
@@ -318,9 +318,9 @@ def change_image2(event):
     #output_path2 = "static/" + save_file2
     print("アウトプットパス: {}".format(output_path))
 
-    img1 = cv2.imread(image_path)     # Load image
-    img1 = imutils.resize(img1, height=500)     # We result in 500px in height
-    mask = get_head_mask(img1)      # We get the mask of the head (without BG)
+    image = cv2.imread(image_path)     # Load image
+    image = imutils.resize(image, height=500)     # We result in 500px in height
+    mask = get_head_mask(image)      # We get the mask of the head (without BG)
 
     # Find the contours, take the largest one and memorize its upper point as the top of the head
     cnts = cv2.findContours(mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)[0]
@@ -332,35 +332,35 @@ def change_image2(event):
     # We remove the face by the color of the skin
     lower = np.array([0, 0, 100], dtype="uint8")  # Lower limit of skin color
     upper = np.array([255, 255, 255], dtype="uint8")  # Upper skin color limit
-    converted = cv2.cvtColor(img1, cv2.COLOR_BGR2HSV)   # We translate into HSV color format
+    converted = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)   # We translate into HSV color format
     skinMask = cv2.inRange(converted, lower, upper)     # Write a mask from places where the color is between the outside
     mask[skinMask == 255] = 0   # We remove the face mask from the mask of the head
 
     kernel1 = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
     mask = cv2.dilate(mask, kernel1, iterations=1)
-    i1 = cv2.bitwise_and(img1, img1, mask=mask)
+    i1 = cv2.bitwise_and(image, image, mask=mask)
 
 
     # If the head is bald, then we deduce that the bald head shows the coordinates of the top point of the head
     if is_bold(topmost,mask):
-        cv2.rectangle(img1,topmost,topmost,(0,0,255),5)
+        cv2.rectangle(image,topmost,topmost,(0,0,255),5)
         print(topmost)
 
     # Otherwise we write that we are not bald and display the coordinates of the largest contour
     else:
         cnts = cv2.findContours(mask.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[0]
         cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
-        cv2.drawContours(img1,[cnts[0]],-1,(0,0,255),2)
+        cv2.drawContours(image,[cnts[0]],-1,(0,0,255),2)
         for c in cnts[0]:
             print(c)
 
     # Display the image in a loop
     while True:
-        cv2.imwrite(output_path, img1)
-        #cv2.imshow("image1", img1)
+        cv2.imwrite(output_path, image)
+        #cv2.imshow("image1", image)
         # Exit to Esc
-        if cv2.waitKey(5) == 27:
-            break
+        # if cv2.waitKey(5) == 27:
+        #     break
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 5000))
