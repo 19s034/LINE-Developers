25af28589abf4d6ec1d65da57dbd96c7e10a3a1e
19s034 <19s034hayate@gmail.com>
2020-11-11 00:55:44 +0000

    a

--- main.py
+++ main.py
@@ -360,7 +360,7 @@ def change_image2(event):
         bgr = cv2.cvtColor(np.array([[[0,255,0]]], dtype=np.uint8), cv2.COLOR_HSV2BGR)[0][0]
         #ポリゴンの領域を塗りつぶす
         
-        test_color = colorsys.hsv_to_rgb(359,1,1)
+        test_color = (round(i * 255) for i in colorsys.hsv_to_rgb(0.5,0.5,0.5))
 
         cv2.fillPoly(image, pts =[cnts[0]], color=test_color)
         #green = np.uint8([[[0,255,0 ]]])
