69d86afaf211bce04be38f5b125235802b6a3481
19s034 <19s034hayate@gmail.com>
2021-01-12 02:36:52 +0000

    a

--- main.py
+++ main.py
@@ -370,7 +370,7 @@ def change_image3(event):
     img2 = cv2.resize(image , (int(width*0.5), int(height*0.5)))
     hsv = cv2.cvtColor(img2, cv2.COLOR_BGR2HSV) # BGR->HSV変換
     hsv_2 = np.copy(hsv)
-    hsv_2[:, :, 0] = np.where((hsv[:, :, 0]>4) & (hsv[:, :, 0]<30) ,hsv[:, :,(2)]*1.5,hsv[:, :, 0])
+    hsv_2[:, :, 0] = np.where((hsv[:, :, 0]>4) & (hsv[:, :, 0]<30) ,hsv[:, :,(2)]*3,hsv[:, :, 0])
     bgr = cv2.cvtColor(hsv_2, cv2.COLOR_HSV2BGR)
     
     
