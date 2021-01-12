47beba3ea299e7c23a23d7eef03eeee2f986b047
19s034 <19s034hayate@gmail.com>
2021-01-12 05:44:57 +0000

    .

--- image_change/skin_change.py
+++ image_change/skin_change.py
@@ -28,7 +28,7 @@ def skin_image(event,userid):
     img2 = cv2.resize(image , (int(width*0.5), int(height*0.5)))
     hsv = cv2.cvtColor(img2, cv2.COLOR_BGR2HSV) # BGR->HSV変換
     hsv_2 = np.copy(hsv)
-    hsv_2[:, :, 0] = np.where((hsv[:, :, 0]>4) & (hsv[:, :, 0]<30) ,hsv[:, :,(2)]*0.075,hsv[:, :, 0])
+    hsv_2[:, :, 0] = np.where((hsv[:, :, 0]>4) & (hsv[:, :, 0]<30) ,hsv[:, :,(2)]*0.75,hsv[:, :, 0])
     #0.001 赤
     #0.3 緑
     bgr = cv2.cvtColor(hsv_2, cv2.COLOR_HSV2BGR)
