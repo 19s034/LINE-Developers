ee460d852002f60d1ca89d3268deb4b78c86b8f7
19s034 <19s034hayate@gmail.com>
2021-01-13 04:41:45 +0000

    a

--- image_change/skin_change.py
+++ image_change/skin_change.py
@@ -17,22 +17,23 @@ def skin_image(event,userid):
  
     image = cv2.imread(image_path)     # Load image
  
-   
+    max = 20
+    diff = 10
+    
+    image_hsv = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+    hsv = np.copy(image_hsv)
+    hsv[:, :, 0] = np.where(image_hsv[:, :, 0] < max, image_hsv[:, :, 0] + diff, image_hsv[:, :, 0])
  
-    HSV_MIN = np.array([0, 30, 30])
-    HSV_MAX = np.array([30, 150, 255])
- 
- 
-    height = image.shape[0]
-    width = image.shape[1]
-    img2 = cv2.resize(image , (int(width*0.5), int(height*0.5)))
-    hsv = cv2.cvtColor(img2, cv2.COLOR_BGR2HSV) # BGR->HSV変換
-    hsv_2 = np.copy(hsv)
-    hsv_2[:, :, 0] = np.where((hsv[:, :, 0]>4) & (hsv[:, :, 0]<30) ,hsv[:, :,(2)]*0.55,hsv[:, :, 0])
-    #0.001 赤
-    #0.3 緑
-    bgr = cv2.cvtColor(hsv_2, cv2.COLOR_HSV2BGR)
+    # height = image.shape[0]
+    # width = image.shape[1]
+    # img2 = cv2.resize(image , (int(width*0.5), int(height*0.5)))
+    # hsv = cv2.cvtColor(img2, cv2.COLOR_BGR2HSV) # BGR->HSV変換
+    # hsv_2 = np.copy(hsv)
+    # hsv_2[:, :, 0] = np.where((hsv[:, :, 0]>4) & (hsv[:, :, 0]<30) ,hsv[:, :,(2)]*0.55,hsv[:, :, 0])
+    # #0.001 赤
+    # #0.3 緑
+    # bgr = cv2.cvtColor(hsv_2, cv2.COLOR_HSV2BGR)
     
 
-    bgr = cv2.cvtColor(hsv_2, cv2.COLOR_HSV2BGR)
+    bgr = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
     cv2.imwrite(output_path, bgr)
