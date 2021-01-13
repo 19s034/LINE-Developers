486048783dea3ac6737c3c2193fa0aea8609bde0
19s034 <19s034hayate@gmail.com>
2021-01-13 05:03:19 +0000

    a

--- image_change/skin_change.py
+++ image_change/skin_change.py
@@ -20,19 +20,25 @@ def skin_image(event,userid):
     max = 20
     diff = 50
     
-    image_hsv = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
-    hsv = np.copy(image_hsv)
-    #hsv[:, :, 0] = np.where(image_hsv[:, :, 0] < max, image_hsv[:, :, 0] + diff, image_hsv[:, :, 0])
-    hsv[:, :, 0] = np.where((image_hsv[:, :, 0]>4) & (image_hsv[:, :, 0]<30), image_hsv[:, :, 0] + diff, image_hsv[:, :, 0])
-    # height = image.shape[0]
-    # width = image.shape[1]
-    # img2 = cv2.resize(image , (int(width*0.5), int(height*0.5)))
-    # hsv = cv2.cvtColor(img2, cv2.COLOR_BGR2HSV) # BGR->HSV変換
-    # hsv_2 = np.copy(hsv)
-    # hsv_2[:, :, 0] = np.where((hsv[:, :, 0]>4) & (hsv[:, :, 0]<30) ,hsv[:, :,(2)]*0.55,hsv[:, :, 0])
-    # #0.001 赤
-    # #0.3 緑
-    # bgr = cv2.cvtColor(hsv_2, cv2.COLOR_HSV2BGR)
+    ang = int(1*(180//16))
+    hsv = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+    hsv = hsv.astype(np.uint16) # uint8だと255以上で不定になるので型を変更
+    hsv[:,:,0] = (hsv[:,:,0]+ang)%180
+    hsv = hsv.astype(np.uint8) # 型を戻す
+
+    # image_hsv = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+    # hsv = np.copy(image_hsv)
+    # #hsv[:, :, 0] = np.where(image_hsv[:, :, 0] < max, image_hsv[:, :, 0] + diff, image_hsv[:, :, 0])
+    # hsv[:, :, 0] = np.where((image_hsv[:, :, 0]>4) & (image_hsv[:, :, 0]<30), image_hsv[:, :, 0] + diff, image_hsv[:, :, 0])
+    # # height = image.shape[0]
+    # # width = image.shape[1]
+    # # img2 = cv2.resize(image , (int(width*0.5), int(height*0.5)))
+    # # hsv = cv2.cvtColor(img2, cv2.COLOR_BGR2HSV) # BGR->HSV変換
+    # # hsv_2 = np.copy(hsv)
+    # # hsv_2[:, :, 0] = np.where((hsv[:, :, 0]>4) & (hsv[:, :, 0]<30) ,hsv[:, :,(2)]*0.55,hsv[:, :, 0])
+    # # #0.001 赤
+    # # #0.3 緑
+    # # bgr = cv2.cvtColor(hsv_2, cv2.COLOR_HSV2BGR)
     
 
     bgr = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
