222a6e33318f4a1fe9566dcea4874fcd3c537c9c
19s034 <19s034hayate@gmail.com>
2021-01-15 05:20:34 +0000

    a

--- main.py
+++ main.py
@@ -41,6 +41,7 @@ def callback():
     return "OK"
 
 def flex(event):
+    print("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
     message = []
     work = event.message.id
     reply_work = event.reply_token
@@ -123,7 +124,7 @@ def handle_message(event):
 
     #肌の色
     elif event.message.text == ">>肌変更" and os.path.exists("static/" + userId):
-        # print("通過: {}".format(event.message.text))
+        print("通過: {}".format(event.message.text))
         # with open(path_w1) as f:
         #     work = f.read()
         # with open(path_w2) as f2:
