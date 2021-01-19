5312e8243919512a8f46581f872acc0a776a0630
19s034 <19s034hayate@gmail.com>
2021-01-19 05:35:03 +0000

    a

--- image_change/skin_change.py
+++ image_change/skin_change.py
@@ -54,7 +54,7 @@ def skin_image(event,userid,color):
         # hsv_4[:, :, 2] = np.where((hsv_3[:, :, 2]>=0) & (hsv_3[:, :, 2]<5) ,hsv_3[:, :, 2] +253,hsv_3[:, :, 2]) #黒色
         # #hsv_4[:, :, 1] = np.where((hsv_3[:, :, 1]>=0) & (hsv_3[:, :, 1]<10) ,hsv_3[:, :, 1] +254,hsv_3[:, :, 1]) #黒色
         
-        hsv_2[:, :, 0] = np.where((hsv[:, :, 0]>6) & (hsv[:, :, 0]<30) ,hsv[:, :, 0] + 160,hsv[:, :, 0]) #ピンク
+        hsv_2[:, :, 0] = np.where((hsv[:, :, 0]>6) & (hsv[:, :, 0]<30) ,hsv[:, :, 0] *0.001,hsv[:, :, 0]) #ピンク
         hsv_3 = np.copy(hsv_2)
         hsv_3[:, :, 2] = np.where((hsv_2[:, :, 0]>= 0) & (hsv_2[:, :, 0]<10) ,hsv_2[:, :, 2] *0.4,hsv_2[:, :, 2]) #黒色
         bgr = cv2.cvtColor(hsv_3, cv2.COLOR_HSV2BGR)
