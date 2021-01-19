2e10752c14b99705cfef387f7708981ec77da1a5
19s034 <19s034hayate@gmail.com>
2021-01-19 02:53:24 +0000

    a

--- image_change/skin_change.py
+++ image_change/skin_change.py
@@ -40,12 +40,15 @@ def skin_image(event,userid,color):
     elif color == 6:
         hsv_2[:, :, 2] = np.where((hsv[:, :, 2]>=0) & (hsv[:, :, 2]<128) ,hsv[:, :, 2] *0.001,hsv[:, :, 2]) #黒色
         hsv_3 = np.copy(hsv_2)
-        hsv_3[:, :, 2] = np.where((hsv_2[:, :, 2]>=0) & (hsv_2[:, :, 2]<128) ,hsv_2[:, :, 2] +150,hsv_2[:, :, 2]) #黒色
+        hsv_3[:, :, 2] = np.where((hsv_2[:, :, 2]>=0) & (hsv_2[:, :, 2]<128) ,hsv_2[:, :, 2] +254,hsv_2[:, :, 2]) #黒色
+        hsv_4 = np.copy(hsv_3)
+        hsv_4[:, :, 1] = np.where((hsv_3[:, :, 1]>=0) & (hsv_3[:, :, 1]<10) ,hsv_3[:, :, 1] +254,hsv_3[:, :, 1]) #黒色
+        bgr = cv2.cvtColor(hsv_4, cv2.COLOR_HSV2BGR)
         #hsv_2[:, :, 2] = np.where((hsv[:, :, 0]>6) & (hsv[:, :, 0]<30) ,hsv[:, :, 2] *0.4,hsv[:, :, 2]) #黒色
     #hsv_2[:, :, 2] = np.where((hsv_2[:, :, 0]>6) & (hsv_2[:, :, 0]<30) ,hsv_2[:, :, 1] *0.7,hsv_2[:, :, 2]) #黒色
     #0.001 赤
     #0.3 緑
 
 
-    bgr = cv2.cvtColor(hsv_3, cv2.COLOR_HSV2BGR)
+    #bgr = cv2.cvtColor(hsv_3, cv2.COLOR_HSV2BGR)
     cv2.imwrite(output_path, bgr)
