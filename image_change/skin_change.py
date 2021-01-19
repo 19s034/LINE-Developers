3350423be39ef3d070a03218778e555de91121ab
19s034 <19s034hayate@gmail.com>
2021-01-19 00:59:05 +0000

    a

--- image_change/skin_change.py
+++ image_change/skin_change.py
@@ -24,22 +24,24 @@ def skin_image(event,userid,color):
     #a = random.randint(1,6)
 
     if color == 1:
-       hsv_2[:, :, 0] = np.where((hsv[:, :, 0]>6) & (hsv[:, :, 0]<30) ,hsv[:, :, 0] + 50,hsv[:, :, 0]) #緑
+        hsv_2[:, :, 0] = np.where((hsv[:, :, 0]>6) & (hsv[:, :, 0]<30) ,hsv[:, :, 0] + 50,hsv[:, :, 0]) #緑
     elif color == 2:
-       hsv_2[:, :, 0] = np.where((hsv[:, :, 0]>6) & (hsv[:, :, 0]<30) ,hsv[:, :, 0] + 100,hsv[:, :, 0]) #青
+        hsv_2[:, :, 0] = np.where((hsv[:, :, 0]>6) & (hsv[:, :, 0]<30) ,hsv[:, :, 0] + 100,hsv[:, :, 0]) #青
     elif color == 3:
-       hsv_2[:, :, 0] = np.where((hsv[:, :, 0]>6) & (hsv[:, :, 0]<30) ,hsv[:, :, 0] + 15,hsv[:, :, 0]) #黄色
+        hsv_2[:, :, 0] = np.where((hsv[:, :, 0]>6) & (hsv[:, :, 0]<30) ,hsv[:, :, 0] + 15,hsv[:, :, 0]) #黄色
     elif color == 4:
-       hsv_2[:, :, 0] = np.where((hsv[:, :, 0]>6) & (hsv[:, :, 0]<30) ,hsv[:, :, 0] + 140,hsv[:, :, 0]) #ピンク
+        hsv_2[:, :, 1] = np.where((hsv[:, :, 1]>0) & (hsv[:, :, 1]<256) ,hsv[:, :, 1] - 120,hsv[:, :, 1]) #ピンク
+        hsv_3 = np.copy(hsv_2)
+        hsv_3[:, :, 0] = np.where((hsv_2[:, :, 0]>6) & (hsv_2[:, :, 0]<30) ,hsv_2[:, :, 0] + 140,hsv_2[:, :, 0]) #ピンク
     elif color == 5:
-       hsv_2[:, :, 0] = np.where((hsv[:, :, 0]>6) & (hsv[:, :, 0]<30) ,hsv[:, :, 0] *0.001,hsv[:, :, 0]) #赤色
+        hsv_2[:, :, 0] = np.where((hsv[:, :, 0]>6) & (hsv[:, :, 0]<30) ,hsv[:, :, 0] *0.001,hsv[:, :, 0]) #赤色
     elif color == 6:
         hsv_2[:, :, 2] = np.where((hsv[:, :, 0]>6) & (hsv[:, :, 0]<30) ,hsv[:, :, 2] *0.4,hsv[:, :, 2]) #黒色
     #hsv_2[:, :, 2] = np.where((hsv_2[:, :, 0]>6) & (hsv_2[:, :, 0]<30) ,hsv_2[:, :, 1] *0.7,hsv_2[:, :, 2]) #黒色
     #0.001 赤
     #0.3 緑
-    bgr = cv2.cvtColor(hsv_2, cv2.COLOR_HSV2BGR)
+    bgr = cv2.cvtColor(hsv_3, cv2.COLOR_HSV2BGR)
     
 
-    bgr = cv2.cvtColor(hsv_2, cv2.COLOR_HSV2BGR)
+    bgr = cv2.cvtColor(hsv_3, cv2.COLOR_HSV2BGR)
     cv2.imwrite(output_path, bgr)
